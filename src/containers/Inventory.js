96d17794308fd736bd552f1c43603151ffca6d59
portgass <petrdsvoboda@gmail.com>
2016-12-03 01:12:19 +0100

    migrated to es6

--- src/containers/Inventory.js
+++ src/containers/Inventory.js
@@ -2,26 +2,25 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-const propTypes = {
-};
-
 class Inventory extends Component {
-  render() {
-    return (
-      <div></div>
-    );
-  }
+    static propTypes = {
+    };
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {};
+    return {};
 };
 const mapDispatchToProps = (dispatch) => {
-  return {};
+    return {};
 };
 
-Inventory.propTypes = propTypes;
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Inventory);
